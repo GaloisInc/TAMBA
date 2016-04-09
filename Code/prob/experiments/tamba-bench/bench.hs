a446eb7b2430f126b1c4c4c89d8a57a6177606e2
José Manuel Calderón Trilla <jmct@galois.com>
2016-04-08 20:01:17 -0400

    Fix timing issue by using the clock package

--- Code/prob/experiments/tamba-bench/bench.hs
+++ Code/prob/experiments/tamba-bench/bench.hs
@@ -1,7 +1,7 @@
 import Text.Printf (printf)
 import System.Process (readProcess)
 import Control.Monad (forM_)
-import Criterion.Measurement (getCPUTime)
+import System.Clock
 import Data.List (isPrefixOf)
 import Statistics.Distribution.Beta
 import Statistics.Distribution (quantile)
@@ -9,23 +9,31 @@ import Control.Parallel
 import Control.DeepSeq
 import Debug.Trace
 
+{-
 time :: IO String -> IO  (String, Double)
 time action = do
   t0 <- getCPUTime
   x <- action
-  rnf x `seq` return ()
+  rnf x `pseq` return ()
   t1 <- getCPUTime
-  return (x, t1 - t0)
+  let t = t1 - t0
+  t `pseq` return (x, t)
+-}
 
 policies :: [String]
 policies = 
   [ 
-  "demo-1d.prob"
+  "demo-1d-5.prob"
   ]
 
+leave :: Int -> [a] -> [a]
+leave n xs = leave' xs (drop n xs)
+  where
+    leave' xs     []     = xs
+    leave' (_:xs) (y:ys) = leave' xs ys
 
 precisions :: [Int]
-precisions = [1]
+precisions = [1..5]
 
 samples :: [Int]
 samples = 0 : 0 : map (10^) [1..7]
@@ -69,7 +77,7 @@ betaHi a b = quantile d (1 - eps)
 makeResult :: String -> Result
 makeResult s = Result (read a) (read b) (read c) (read d) (read e) (read f) (read g) (read h) (read i)
   where
-  vals = map ((!! 2) . words) . dropWhile (not . isPrefixOf "size_z") $ lines s
+  vals = map ((!! 2) . words) . leave 9 $ lines s
   [a,b,c,d,e,f,g,h,i] = vals
 
 -- byFile = do
@@ -92,10 +100,15 @@ byPrec = do
     forM_ precisions $ \pr -> do
       forM_ samples $ \n -> do
   
-        (output, t) <- time $ readProcess "bash" ["-c", makeCommand pol pr n] ""
+        t0 <- getTime Monotonic
+        output <- readProcess "bash" ["-c", makeCommand pol pr n] ""
+        rnf output `pseq` return ()
+        t1 <- getTime Monotonic
+        let t = (fromIntegral $ toNanoSecs $ diffTimeSpec t1 t0 :: Double) / (10^9)
         --putStrLn output
         let result = makeResult output
         let (lo, hi) = bounds result
         printf "%d, %.3e, %.5f, %.3e, %.3e, %.3e\n" pr (fromIntegral n :: Double) t lo hi (size result)
+
 
 main = byPrec
